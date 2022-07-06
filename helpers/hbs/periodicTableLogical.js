581819a855d07d42ebfa037f788c2abb1fadda94
luisferllub230 <84060723+luisferllub230@users.noreply.github.com>
2022-07-06 16:05:01 -0400

    type: bugs solvents

--- helpers/hbs/periodicTableLogical.js
+++ helpers/hbs/periodicTableLogical.js
@@ -1,20 +1,6 @@
 
 let container = false;
-const lookingForCharacter = (value1) => { 
-    let callback = (value2) => {
-        value1 === "0" ?  container = true : container = false;
-    }
-    callback(value1);
-    return container;
-}
+const lookingForCharacter = value1 =>value1 === "404" ?  true : false;
 
-const noLookingForCharacter = (value1) => { 
-    let callback = (value2) => {
-        value1 !== "0" ?  container = true : container = false;
-    }
-    callback(value1);
-    return container;
-}
 
-exports.lookingForCharacter = lookingForCharacter;
-exports.noLookingForCharacter = noLookingForCharacter;
+exports.lookingForCharacter = lookingForCharacter; //
